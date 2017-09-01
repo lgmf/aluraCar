95bfccce1996312928301b4ad308146d9775c5ec
Luiz Guilherme Ferrari <lgm.ferrari@hotmail.com>
2017-09-01 20:49:20 -0300

    Aulas 2 e 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,33 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { SelectedCarPage } from '../pages/selected-car/selected-car';
+
+import { HttpModule } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    SelectedCarPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    SelectedCarPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,53 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
+import { Http } from '@angular/http';
+
+import { SelectedCarPage } from '../selected-car/selected-car';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor(public navCtrl: NavController) {
+  public cars : any = [];
 
+  constructor(
+    public navCtrl: NavController,
+    private _http: Http,
+    private _loadingCtrl: LoadingController,
+    private _alertCtrl: AlertController) { }
+
+  ngOnInit() {
+
+    let loader = this._loadingCtrl.create({
+      content: 'Buscando novos carros. Aguarde ...'
+    });
+
+    loader.present();
+
+    this._http
+      .get('https://aluracar.herokuapp.com/')
+      .map(res => res.json())
+      .toPromise()
+      .then(carros => {
+        this.cars = carros;
+        loader.dismiss();
+      })
+      .catch(err => {
+        console.log(err);
+        loader.dismiss();
+        this._alertCtrl
+          .create({
+            title: 'Falha na conexão',
+            buttons: [{ text: 'Estou ciente!' }],
+            subTitle: 'Não foi possível obter a lista de carros. Tente mais tarde.'
+          }).present();
+      });
+  }
+
+  getCar(car) {
+    this.navCtrl.push(SelectedCarPage, { selectedCar: car });
   }
 
 }
